fb04f6c58f2a0ca86c98db3007af3f49dfcb36c1
Wisp <zk1019@outlook.com>
2022-12-19 02:06:23 +0800

    Fixed CQP

--- jm/jm_auto_all.py
+++ jm/jm_auto_all.py
@@ -117,8 +117,10 @@ def generate(qp, rawName, rawFrames):
     IntraDelay            = 0   # Intra (IDR) picture delay (i.e. coding structure of PPIPPP... )
     EnableIDRGOP          = 0   # Support for IDR closed GOPs (0: disabled, 1: enabled)
     EnableOpenGOP         = 0   # Support for open GOPs (0: disabled, 1: enabled)
-    QPISlice              = 15  # Quant. param for I Slices (0-51)
-    QPPSlice              = 15  # Quant. param for P Slices (0-51)
+    '''
+    file.write('QPISlice              = ' + qp + '  # Quant. param for I Slices (0-51)\n')
+    file.write('QPPSlice              = ' + qp + '  # Quant. param for P Slices (0-51)\n')
+    '''
     FrameSkip             = 0   # Number of frames to be skipped in input (e.g 2 will code every third frame). 
                                 # Note that this now excludes intermediate (i.e. B) coded pictures
     ChromaQPOffset        = 0   # Chroma QP offset (-51..51)
@@ -220,7 +222,9 @@ def generate(qp, rawName, rawFrames):
 
     NumberBFrames          = 8  # Number of B coded frames inserted (0=not used)
     PReplaceBSlice         = 0  # Replace B-coded slices with P-coded slices when NumberBFrames>0
-    QPBSlice               = 30 # Quant. param for B slices (0-51)
+    '''
+    file.write('QPBSlice               = ' + qp + ' # Quant. param for B slices (0-51)')
+    '''
     BRefPicQPOffset        = -1 # Quantization offset for reference B coded pictures (-51..51)
     DirectModeType         = 1  # Direct Mode Type (0:Temporal 1:Spatial)
     DirectInferenceFlag    = 1  # Direct Inference Flag (0: Disable 1: Enable)
@@ -543,7 +547,7 @@ def generate(qp, rawName, rawFrames):
 
     CbQPOffset              = 0     # Chroma QP offset for Cb-part (-51..51)
     CrQPOffset              = 0     # Chroma QP offset for Cr-part (-51..51)
-    Transform8x8Mode        = 1     # (0: only 4x4 transform, 1: allow using 8x8 transform additionally, 2: only 8x8 transform)
+    Transform8x8Mode        = 0     # (0: only 4x4 transform, 1: allow using 8x8 transform additionally, 2: only 8x8 transform)
     ReportFrameStats        = 0     # (0:Disable Frame Statistics 1: Enable)
     DisplayEncParams        = 0     # (0:Disable Display of Encoder Params 1: Enable)
     Verbose                 = 1     # level of display verboseness 
@@ -779,7 +783,7 @@ shellFile     = open(shellFileName, mode='w', encoding='utf-8')
 shellFile.write('mkdir /data/WispChan/jm_benchmark_output/' + rawNameSplit[0] + '\n')
 for b in QPList:
     generate(b, rawName, rawFrames)
-    shellFile.write('./lencod.exe -f ' + rawNameSplit[0] + '_' + b + '.cfg | tee ' + rawNameSplit[0] + '_' + b + '.log &\n')
+    shellFile.write('./lencod.exe -f ' + rawNameSplit[0] + '_' + b + '.cfg > ' + rawNameSplit[0] + '_' + b + '.log &\n')
     # if b!='41':
     #     shellFile.write(' && ')
 shellFile.close()
